b0e6800b330a4f77f948edafe2bad5478e858bac
SandroKE <koomealessandro@gmail.com>
2024-02-21 02:50:28 -0700

    Simple react-native code to test app on using EXPO GO

--- app/sacco-vote-app/App.js
+++ app/sacco-vote-app/App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+//using react native image components to display images
+const PlaceholderImage = require('./assets/images/background_image.jpg');
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First Front end page Sacco Vote</Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
