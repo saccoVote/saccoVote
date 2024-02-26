6a6e347a6b92b1f503f492604bda67a844e7d884
SandroKE <koomealessandro@gmail.com>
2024-02-26 07:19:53 -0700

    Working on React Navigation

--- app/sacco-vote-app/App.js
+++ app/sacco-vote-app/App.js
@@ -1,47 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/Home';
+import SignupScreen from './screens/Sign_up';
 
-//importing all necessary components
-import Button from './components/Button';
-import ImageViewer from './components/ImageViewer';
+const Stack = createStackNavigator();
 
-//using react native image components to display images
-const PlaceholderImage = require('./assets/images/background_image.jpg');
-
-
-
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
-});
+export default App;
