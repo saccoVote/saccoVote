3fb8a6f8b095fb267b433920e536266b1a2b027b
Eve Candy Mutunga <evecandymutunga@gmail.com>
2024-04-05 22:01:58 +0300

    make user emails case insensitive

--- saccoVoteAPI/base/authentication.py
+++ saccoVoteAPI/base/authentication.py
@@ -6,7 +6,7 @@ class EmailBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
         user_model = get_user_model()
         try:
-            user = user_model.objects.get(email=email)
+            user = user_model.objects.get(email__iexact=email)
         except user_model.DoesNotExist:
             return None
         if user.check_password(password):
