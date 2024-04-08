c7961a6e1b0c0d6d9498120b57601ab635565efa
Eve Candy Mutunga <evecandymutunga@gmail.com>
2024-04-08 09:50:26 +0300

    API updates and fixes

--- saccoVoteAPI/base/views/vetting_view_set.py
+++ saccoVoteAPI/base/views/vetting_view_set.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from base.custom_permissions import IsSaccoVetter
-from base.models import SaccoUser, Election
+from base.models import SaccoUser, Election, ElectionCandidate
 from base.models.vetting import Vetting
 from base.serializers import CreateVettingSerializer, VettingSerializer
 
@@ -20,17 +20,21 @@ class VettingViewSet(CreateModelMixin, GenericViewSet):
         election = get_object_or_404(Election, pk=kwargs.get('election_pk'), sacco_id=kwargs.get('sacco_pk'))
         if election.has_not_started():
             vetter = get_object_or_404(SaccoUser, user=request.user)
-            candidate = get_object_or_404(SaccoUser, user=request.data.get('candidate'))
+            candidate = get_object_or_404(ElectionCandidate, pk=kwargs.get('candidate_pk'))
 
-            serializer = VettingSerializer(data={
-                'vetter': vetter.id,
-                'candidate': candidate.id,
-                'election': election.id,
-                'approved': request.data.get('approved')
-            })
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            # serializer = VettingSerializer(data={
+            #     'vetter': vetter.id,
+            #     'candidate': candidate.sacco_user_id,
+            #     'election': election.id,
+            #     'approved': request.data.get('approved')
+            # })
+            try:
+                Vetting.objects.create(
+                    vetter=vetter, candidate=candidate,
+                    election=election, approved=request.data.get('approved')
+                )
+                return Response({'message': 'Candidate Vetted'}, status=status.HTTP_201_CREATED)
+            except:
+                return Response({'error': 'Unable to vet candidate'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'error': 'Vetting can only be done on future elections'},
                         status=status.HTTP_422_UNPROCESSABLE_ENTITY)
--- saccoVoteAPI/base/views/vote_view_set.py
+++ saccoVoteAPI/base/views/vote_view_set.py
@@ -7,9 +7,11 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from base.custom_permissions import IsSaccoMember
-from base.models import ElectionCandidate, Election, VoteActivity
+from base.models import ElectionCandidate, Election, VoteActivity, SaccoUser
 from base.models.vote import Vote
 from base.serializers import VoteSerializer, CreateVoteSerializer, ElectionCandidateSerializer
+
+from django.db import transaction
 
 
 class VoteViewSet(CreateModelMixin, ListModelMixin, GenericViewSet):
@@ -29,21 +31,24 @@ class VoteViewSet(CreateModelMixin, ListModelMixin, GenericViewSet):
             return CreateVoteSerializer
         return VoteSerializer
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         request.data['election'] = self.kwargs.get('election_pk')
-        serializer = VoteSerializer(data=request.data)
-        if serializer.is_valid():
+        # serializer = VoteSerializer(data=request.data)
+        if request.data.get('candidate'):
+            voter = get_object_or_404(SaccoUser, user=request.user)
             election = get_object_or_404(Election, pk=self.kwargs.get('election_pk'))
             candidate = get_object_or_404(ElectionCandidate, pk=request.data.get('candidate'))
             user_has_voted = VoteActivity.objects.filter(voter__user__id=request.user.id, election=election).exists()
             if user_has_voted:
                 return Response({'error': 'User has already voted in this election'}, status=status.HTTP_409_CONFLICT)
             if election.is_ongoing():
-                if ElectionCandidateSerializer(candidate).is_approved:
-                    self.perform_create(serializer)
+                if ElectionCandidateSerializer(candidate, context={'request': request}).get_is_approved(candidate):
+                    VoteActivity.objects.create(election=election, voter=voter)
+                    Vote.objects.create(election=election, candidate=candidate)
                     return Response({'message': 'Vote successful'}, status=status.HTTP_201_CREATED)
                 return Response({'error': 'Candidate is not vetted for this election'},
                                 status=status.HTTP_422_UNPROCESSABLE_ENTITY)
             return Response({'error': 'Election is not ongoing'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'unable to vote'}, status=status.HTTP_400_BAD_REQUEST)
 
